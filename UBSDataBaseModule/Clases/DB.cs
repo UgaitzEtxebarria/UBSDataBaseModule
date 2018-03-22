4634e744acc815ce5d75b5a9895520dd535d3526
Ugaitz Etxebarria <etxeba17@gmail.com>
2018-03-22 17:20:40 +0100

    Creation of the project

---
+++ UBSDataBaseModule/Clases/DB.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace UBSDataBaseModule.Clases
+{
+    public class DB
+    {
+        private UBSLib.UBSBackgroundModule parent;
+        private ConcurrentQueue<DBData> pilaComandos;
+        private string connectionString;
+        private string defaultConnectionString = "Data Source=.\\DB\\";
+
+        Thread thrCambioProd;
+        private System.Timers.Timer tmr;
+        private bool cambiarDB;
+        private bool cerrarDB;
+        private ConcurrentQueue<SQLiteTransaction> pilaTransacciones;
+        private SQLiteTransaction actualTransaction = null;
+
+        /////////////////////////////////////////////////////////////////////////////
+        /////
+
+        public DB(string dbname, UBSLib.UBSBackgroundModule parent)
+        {
+            pilaComandos = new ConcurrentQueue<DBData>();
+            this.parent = parent;
+            cambiarDB = false;
+            cerrarDB = false;
+
+            string fecha = DateTime.Now.ToString().Replace('/', '-').Replace(':', '.');
+
+            this.connectionString = "Data Source=.\\BD\\" + dbname + " (" + fecha + ").db;Version=3; PRAGMA synchronous=OFF;";
+
+            SQLiteConnection.CreateFile(".\\BD\\" + dbname + " (" + fecha + ").db");
+
+            SQLiteConnection connection = new SQLiteConnection(connectionString);
+
+            pilaTransacciones = new ConcurrentQueue<SQLiteTransaction>();
+
+            try
+            {
+                connection.Open();
+
+                DateTime t1 = DateTime.Now;
+                parent.Log("Creacion de las tablas de la base de datos iniciada : " + t1);
+
+                
+
+                parent.Log("Creacion de las tablas finalizada : +" + (DateTime.Now - t1).TotalMilliseconds);
+                connection.Close();
+
+                tmr = new System.Timers.Timer();
+                tmr.Interval = 5000;
+                tmr.Elapsed += ActivarGuardado;
+                tmr.Enabled = true;
+                tmr.Start();
+            }
+            catch (Exception e)
+            {
+                parent.Error("Error al crear la base de datos. " + e.Message, true, false);
+            }
+            finally
+            {
+                connection.Dispose();
+            }
+
+            thrCambioProd = new Thread(() => ejecutorComandos());
+            thrCambioProd.Name = "DB";
+            thrCambioProd.IsBackground = true;
+            thrCambioProd.Start();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        public void ejecutorComandos()
+        {
+            SQLiteConnection connection;
+            DBData dataDB = null;
+            while (!cerrarDB || (cerrarDB && (pilaComandos.Count > 0)))
+            {
+                cambiarDB = false;
+                //parent.Log("(" + DateTime.Now.ToString("HH:mm:ss.FFF") + ") Intento de leer pila, objetos en la pila: " + pilaComandos.Count, false);
+                if (pilaComandos.Count > 0)
+                {
+                    if (pilaComandos.TryPeek(out dataDB))
+                    {
+                        //parent.Log("(" + DateTime.Now.ToString("HH:mm:ss.FFF") + ") Nueva orden recibida, objetos en la pila: " + pilaComandos.Count, false);
+                        connection = new SQLiteConnection(dataDB.Connection);
+                        try
+                        {
+                            connection.Open();
+
+                            actualTransaction = connection.BeginTransaction();
+                            while (!cambiarDB)
+                            {
+                                DateTime t1 = DateTime.Now;
+                                if (pilaComandos.TryPeek(out dataDB))
+                                {
+                                    if (dataDB.Connection != connection.ConnectionString)
+                                    {
+                                        cambiarDB = true;
+                                        break;
+                                    }
+                                    //parent.Log("(" + DateTime.Now.ToString("HH:mm:ss.FFF") + ") Nueva orden valida, objetos en la pila: " + pilaComandos.Count, false);
+
+                                    if (pilaComandos.TryDequeue(out dataDB))
+                                    {
+                                        using (var command = new SQLiteCommand(connection))
+                                        {
+                                            //parent.Log("(" + DateTime.Now.ToString("HH:mm:ss.FFF") + ") Orden " + dataDB.Command + " recibida en " + t1.ToString("HH:mm:ss.FFF") + ", objetos en la pila: " + pilaComandos.Count, false);
+                                            command.CommandText = dataDB.Command;
+                                            if (!dataDB.Command.StartsWith("INSERT") && !dataDB.Command.StartsWith("UPDATE") && !dataDB.Command.StartsWith("DELETE"))
+                                            {
+                                                List<string> Selected = new List<string>();
+                                                DbDataReader reader = command.ExecuteReader();
+
+                                                //parent.Log("(" + DateTime.Now.ToString("HH:mm:ss.FFF") + ") Orden " + dataDB.Command + " enviada en " + (DateTime.Now - t1).TotalMilliseconds + " ms, objetos en la pila: " + pilaComandos.Count, false);
+
+                                                while (reader.Read())
+                                                {
+                                                    Object[] objs = new Object[reader.FieldCount];
+                                                    reader.GetValues(objs);
+
+                                                    foreach (object obj in objs)
+                                                        Selected.Add(obj.ToString());
+                                                }
+                                                //parent.Log("(" + DateTime.Now.ToString("HH:mm:ss.FFF") + ") Orden " + dataDB.Command + " datos conseguidos en " + (DateTime.Now - t1).TotalMilliseconds + " ms, objetos en la pila: " + pilaComandos.Count, false);
+                                                dataDB.Data = String.Join("#", Selected.ToArray());
+                                                ((DBModule)parent).DBResults[dataDB.Variable] = dataDB;
+                                                dataDB.Readed = true;
+                                                //parent.WriteConsole("(" + DateTime.Now.ToString("HH:mm:ss.FFF") + ") Dato guardado: " + dataDB.Command + " en " + (DateTime.Now - t1).TotalMilliseconds + " ms, objetos en la pila: " + pilaComandos.Count, true);
+                                                //parent.SetGlobalParameter(dataDB.Variable, dataDB);
+                                                //parent.Log("(" + DateTime.Now.ToString("HH:mm:ss.FFF") + ") Orden " + dataDB.Command + " datos globales guardados en " + (DateTime.Now - t1).TotalMilliseconds + " ms, objetos en la pila: " + pilaComandos.Count, false);
+                                            }
+                                            else
+                                                command.ExecuteNonQuery();
+
+                                            /*if (dataDB != null)
+                                                parent.Log("(" + DateTime.Now.ToString("HH:mm:ss.FFF") + ") Orden " + dataDB.Command + " completada en " + (DateTime.Now - t1).TotalMilliseconds + " ms, objetos en la pila: " + pilaComandos.Count, false);*/
+                                        }
+                                    }
+                                }
+                            }
+                            //connection.Close();
+                        }
+                        catch (Exception e)
+                        {
+                            parent.WriteConsole("Alarma 10002 activada" + ((dataDB.Command != null) ? (" con la orden " + dataDB.Command) : "") + " por: " + e.Message, true);
+                        }
+                        finally
+                        {
+                            //connection.Dispose();
+                        }
+                    }
+                }
+                if (cambiarDB && actualTransaction.Connection != null)
+                {
+                    //Cerrar BD en otro hilo ya que tarda mas de lo esperado.
+                    Thread thrCerrarDB = new Thread(() => CerrarBaseDeDatos(actualTransaction));
+                    thrCerrarDB.Name = "closeDB";
+                    thrCerrarDB.IsBackground = true;
+                    thrCerrarDB.Start();
+                    //parent.Log("(" + DateTime.Now.ToString("HH:mm:ss.FFF") + ") Orden mandada a commitear en " + DateTime.Now.ToString("HH:mm:ss.FFF") + ", objetos en la pila: " + pilaComandos.Count, false);
+                }
+                Thread.Sleep(1);
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        public void ActivarGuardado(object sender, EventArgs e)
+        {
+            if (Thread.CurrentThread.Name == null)
+                Thread.CurrentThread.Name = "Guardador de BD";
+            cambiarDB = true;
+            tmr.Stop();
+            tmr.Enabled = true;
+            tmr.Start();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        public void Destroy()
+        {
+            cerrarDB = true;
+            thrCambioProd.Join();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        public void CerrarBaseDeDatos(SQLiteTransaction transaction)
+        {
+            SQLiteTransaction dequeued = null;
+            try
+            {
+                //parent.WriteConsole("Cantidad en la fila de transacciones: " + pilaTransacciones.Count, true);
+                if (transaction != null)
+                    pilaTransacciones.Enqueue(transaction);
+                else
+                    pilaTransacciones.Enqueue(actualTransaction);
+
+                while (pilaTransacciones.Count > 0)
+                {
+                    if (pilaTransacciones.TryDequeue(out dequeued))
+                        transaction.Commit();
+                }
+            }
+            catch (Exception e)
+            {
+                parent.Error("Error al cerrar la base de datos. " + e.Message, true, false);
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+        public void añadirPila(string command, string nameDB, string variable)
+        {
+            try
+            {
+                string connection = "";
+
+                if (nameDB == "")
+                    connection = connectionString;
+                else
+                    connection = defaultConnectionString + nameDB + ".db;Version=3;";
+
+                DBData result = new DBData(connection, command, variable);
+
+                añadirPila(result);
+            }
+            catch (Exception e)
+            {
+                parent.Error("Error al lanzar una llamada select en la base de datos. " + e.Message, true, false);
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+        public void añadirPila(DBData dataDB)
+        {
+            if (!cerrarDB)
+            {
+                try
+                {
+                    if (dataDB.Variable != "")
+                    {
+                        Stopwatch sw = new Stopwatch();
+
+                        sw.Start();
+                        while (((DBModule)parent).DBResults.ContainsKey(dataDB.Variable) && sw.Elapsed.TotalMilliseconds < 100)
+                        {
+                            parent.Log("DB => la variable " + dataDB.Variable + " ya existe, en espera...");
+                            Application.DoEvents();
+                            Thread.Sleep(1);
+                        }
+                        sw.Stop();
+
+                        if (((DBModule)parent).DBResults.ContainsKey(dataDB.Variable))
+                            parent.Error("Una peticion se ha quedado residual.", true, false);
+                        else
+                            //parent.WriteConsole("Añadiendo a la pila de BD: " + dataDB.Command, true);
+                            ((DBModule)parent).DBResults.Add(dataDB.Variable, dataDB);
+                    }
+                }
+                catch (Exception e)
+                {
+                    parent.Error("Error al crear el pedido en la base de datos. " + e.Message, true, false);
+                }
+
+                try
+                {
+                    pilaComandos.Enqueue(dataDB);
+                }
+                catch (Exception e)
+                {
+                    parent.Error("Error al añadir el pedido en base de datos. " + e.Message, true, false);
+                }
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+        public bool insert(string Tabla, string Variables, string Valores)
+        {
+            return insert(Tabla, Variables, Valores, "");
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        public bool insert(string Tabla, string Variables, string Valores, string nameDB)
+        {
+            if (Variables.Split(',').Length != Valores.Split(',').Length) return false;
+            string str = "INSERT INTO " + Tabla + " (" + Variables + ") VALUES (" + Valores + ")";
+
+            añadirPila(str, nameDB, "");
+
+            return true;
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        public bool update(string Tabla, string Set, string Condition)
+        {
+            return update(Tabla, Set, Condition, "");
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        public bool update(string Tabla, string Set, string Condition, string nameDB)
+        {
+            string str = "UPDATE " + Tabla + " SET " + Set + " WHERE " + Condition;
+
+            añadirPila(str, nameDB, "");
+
+            return true;
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        public bool delete(string Tabla, string Condition)
+        {
+            return delete(Tabla, Condition, "");
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        public bool delete(string Tabla, string Condition, string nameDB)
+        {
+            string str = "DELETE FROM " + Tabla + " WHERE " + Condition;
+            añadirPila(str, nameDB, "");
+            return true;
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        public bool select(string Tabla, string Variables, string where, string returnVar)
+        {
+            return select(Tabla, Variables, where, "", returnVar);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        public bool select(string Tabla, string Variables, string where, string nameDB, string returnVar)
+        {
+            try
+            {
+                string str = "select " + Variables + " from " + Tabla + ((where != "") ? " where " + where : "");
+
+                añadirPila(str, nameDB, returnVar);
+                return true;
+            }
+            catch (Exception e)
+            {
+                parent.Error("Error al lanzar una llamada select en la base de datos. " + e.Message, true, false);
+                return false;
+            }
+
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        public bool free(string text, string returnVar)
+        {
+            return free(text, "", returnVar);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        public bool free(string text, string nameDB, string returnVar)
+        {
+            añadirPila(text, nameDB, returnVar);
+            return true;
+        }
+    }
+}
---
+++ UBSDataBaseModule/Modulo/DBModule.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+using UBSDataBaseModule.Clases;
+
+namespace UBSDataBaseModule
+{
+    public class DBData
+    {
+        public DBData(string connection, string command, string variable)
+        {
+            Connection = connection;
+            Command = command;
+            Variable = variable;
+            Data = "";
+            Readed = false;
+        }
+
+        public string Connection { get; set; }
+
+        public string Command { get; set; }
+
+        public string Variable { get; set; }
+
+        public string Data { get; set; }
+
+        public bool Readed { get; set; }
+    }
+
+    public class DBModule : UBSLib.UBSBackgroundModule
+    {
+        private DB db;
+        private Dictionary<string, DBData> results;
+
+        private object lockDB;
+
+        //////////////////////////////////////////////////////////////////////////////////
+        public DBModule(string _id)
+            : base(_id)
+        { }
+
+
+        //////////////////////////////////////////////////////////////////////////////////
+        public override bool Init()
+        {
+            base.Init();
+            db = new DB("Dummy", this);
+            results = new Dictionary<string, DBData>();
+
+            lockDB = new object();
+
+            WriteConsole("Módulo cargado correctamente.");
+            return true;
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+        public override bool Destroy()
+        {
+            db.Destroy();
+            WriteConsole("Apagado de la base de datos: " + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.FFF"), true);
+            return true;
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+        public override void HandleMessages(UBSLib.UBSMessage message)
+        {
+            string strMessage = message.ToString();
+
+            //Mensajes
+            string[] strParams = strMessage.Split('#');
+
+            if (strMessage.StartsWith("insert#"))
+            {
+                try
+                {
+                    string Tabla = strParams[1];
+                    string vars = strParams[2];
+                    string vals = strParams[3];
+
+                    if (strParams.Length == 4)
+                    {
+                        if (!db.insert(Tabla, vars, vals))
+                            Error("No se ha podido llevar a cabo el proceso Insert en la base de datos.", true, false);
+                    }
+                    else
+                    {
+                        string DB = strParams[4];
+                        if (!db.insert(Tabla, vars, vals, DB))
+                            Error("No se ha podido llevar a cabo el proceso Insert en la base de datos.", true, false);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Error("La estructura de la llamada para el proceso Insert está mal estructurada. " + e.Message, true, false);
+                }
+            }
+
+            if (strMessage.StartsWith("update#"))
+            {
+                try
+                {
+                    string Tabla = strParams[1];
+                    string sets = strParams[2];
+                    string cond = strParams[3];
+
+                    if (!db.update(Tabla, sets, cond))
+                        Error("No se ha podido llevar a cabo el proceso Update en la base de datos.", true, false);
+                }
+                catch (Exception e)
+                {
+                    Error("La estructura de la llamada para el proceso Update está mal estructurada. " + e.Message, true, false);
+                }
+            }
+
+            if (strMessage.StartsWith("delete#"))
+            {
+                try
+                {
+                    string Tabla = strParams[1];
+                    string cond = strParams[2];
+
+                    if (strParams.Length == 3)
+                    {
+                        if (!db.delete(Tabla, cond))
+                            Error("No se ha podido llevar a cabo el proceso Delete en la base de datos.", true, false);
+                    }
+                    else
+                    {
+                        string DB = strParams[3];
+                        if (!db.delete(Tabla, cond, DB))
+                            Error("No se ha podido llevar a cabo el proceso Delete en la base de datos.", true, false);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Error("La estructura de la llamada para el proceso Delete está mal estructurada. " + e.Message, true, false);
+                }
+            }
+
+            if (strMessage.StartsWith("select#"))
+            {
+                try
+                {
+                    string Variable = strMessage;
+                    string Tabla = strParams[1];
+                    string vars = strParams[2];
+                    string where = strParams[3];
+
+                    List<string> listVal = new List<string>();
+
+                    if (strParams.Length == 4)
+                    {
+                        if (!db.select(Tabla, vars, where, Variable))
+                            Error("No se ha podido llevar a cabo el proceso Select en la base de datos.", true, false);
+                    }
+                    else
+                    {
+                        string DB = strParams[4];
+                        if (!db.select(Tabla, vars, where, DB, Variable))
+                            Error("No se ha podido llevar a cabo el proceso Select en la base de datos.", true, false);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Error("La estructura de la llamada para el proceso Select está mal estructurada. " + e.Message, true, false);
+                }
+            }
+
+            if (strMessage.StartsWith("free#"))
+            {
+                try
+                {
+                    List<string> listVal = new List<string>();
+                    string Variable = strMessage;
+                    string text = strParams[1];
+
+                    if (strParams.Length == 3)
+                    {
+                        string DB = strParams[2];
+                        if (!db.free(text, DB, Variable))
+                            Error("No se ha podido llevar a cabo el texto libre en la base de datos.", true, false);
+                    }
+                    else if (strParams.Length == 2)
+                    {
+                        if (!db.free(text, Variable))
+                            Error("No se ha podido llevar a cabo el texto libre en la base de datos.", true, false);
+                    }
+                    else
+                    {
+                        Error("Los parametros de la llamada a la base de datos no concuerdan.", true, false);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Error("La estructura de la llamada a la base de datos de forma libre está mal estructurada. " + e.Message, true, false);
+                }
+            }
+
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+
+        public string getData(string Variable)
+        {
+            try
+            {
+                string result = "";
+                int intentos = 0;
+                bool found = false;
+                DateTime t1 = DateTime.Now;
+
+                if (results.ContainsKey(Variable))
+                {
+                    while (!found)
+                    {
+                        Stopwatch sw = new Stopwatch();
+
+                        sw.Start();
+                        DBData dataDB = results[Variable];
+                        bool auxReaded = false;
+                        while (!auxReaded && sw.Elapsed.Seconds < 2)
+                        {
+                            auxReaded = dataDB.Readed;
+
+                            if (auxReaded)
+                                break;
+                            Application.DoEvents();
+                            Thread.Sleep(1);
+                        }
+                        Log("Conseguir el dato " + Variable + " de la BD: " + sw.Elapsed.TotalMilliseconds);
+                        sw.Stop();
+
+                        result = results[Variable].Data;
+
+                        results.Remove(Variable);
+
+                        if (!auxReaded && intentos < 3)
+                        {
+                            db.añadirPila(dataDB);
+                            intentos++;
+                            WriteConsole("No se ha podido leer el dato " + Variable + " de la base de datos, reenviando la orden " + dataDB.Command + " (" + intentos + "/3)", true);
+                        }
+                        else
+                            found = true;
+                    }
+                }
+                else
+                    Error("No se encuentra el query " + Variable, true, false);
+                return result;
+            }
+            catch (Exception e)
+            {
+                Error("Error al recibir el resultado de la base de datos. " + e.Message, true, false);
+                return null;
+            }
+        }
+
+        public Dictionary<string, DBData> DBResults
+        {
+            get
+            {
+                lock (lockDB)
+                {
+                    return results;
+                }
+            }
+        }
+    }
+}
+
